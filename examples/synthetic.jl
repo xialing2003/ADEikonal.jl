c2c7f43e7b7a4e9da262c1def13ee61aef077204
xialing2003 <xl2524694262@mail.ustc.edu.cn>
2024-05-12 20:41:46 -0700

    generate test data and corresponding code

--- examples/synthetic.jl
+++ examples/synthetic.jl
@@ -1,3 +1,4 @@
+push!(LOAD_PATH,"../src")
 using CSV
 using DataFrames
 using HDF5
@@ -9,12 +10,16 @@ using JSON
 using Dates
 Random.seed!(123)
 
+folder = "anomaly"
+# folder = "checkerboard"
+# len = 10
+
 ## define research region
 config = Dict(
            "minlatitude" => 30.0,
-           "maxlatitude" => 31.0,
+           "maxlatitude" => 30.5,
            "minlongitude" => 130.0,
-           "maxlongitude" => 131.0,
+           "maxlongitude" => 130.5,
            "mindepth" => 0.0,
            "maxdepth" => 20.0,
            "degree2km" => 111.19
@@ -24,8 +29,10 @@ time0 = DateTime("2019-01-01T00:00:00", "yyyy-mm-ddTHH:MM:SS")
 config["depth0"] = (config["mindepth"] + config["maxdepth"]) / 2
 config["latitude0"] = (config["minlatitude"] + config["maxlatitude"]) / 2
 config["longitude0"] = (config["minlongitude"] + config["maxlongitude"]) / 2
+config["latituderange"] = config["maxlatitude"] - config["minlatitude"]
+config["longituderange"] = config["maxlongitude"] - config["minlongitude"]
 
-open("config.json", "w") do f
+open(folder * "/config.json", "w") do f
     n = length(config); i = 0
     println(f, "{")
     for (key, value) in config
@@ -37,6 +44,41 @@ open("config.json", "w") do f
     println(f, "}")
 end
 
+pyproj = pyimport("pyproj") # use it in an environment with pyproj
+proj = pyproj.Proj(
+    "+proj=sterea +lon_0=$( (config["minlongitude"] + config["maxlongitude"]) / 2 ) +lat_0=$( (config["minlatitude"] + config["maxlatitude"]) / 2 ) +units=km"
+)
+dx, dy = proj(config["minlongitude"],config["minlatitude"])
+dx = ceil(abs(dx)) + 2; dy = ceil(abs(dy)) + 2; dz = 1; h = 1.0
+
+m, n = proj(config["maxlongitude"], config["maxlatitude"])
+m = convert(Int64,ceil((m + dx)/h)) + 3
+n = convert(Int64, ceil((n + dy)/h)) + 3
+l = convert(Int64, ceil((config["maxdepth"]+dz)/h)) + 1
+
+## calculate traveltime of stations and events
+
+vel_p = ones(m,n,l) * 6.0; vel_s = ones(m,n,l) * 3.5 # (54, 61, 22)
+if folder == "anomaly"
+    vel_p[20:40, 20:40, 8:14] .= 6.5; vel_s[20:50, 20:40, 8:14] .= 3.8
+    vel_p[5:25, 4:19, 2:7] .= 5.5; vel_s[5:25, 4:19, 2:7] .= 3.2
+elseif folder == "checkerboard"
+    for i = 0:m-1
+        for j = 0:n-1
+            for k = 0:l-1
+                ii = (i-i%len)/len; jj = (j-j%len)/len; kk = (k-k%len)/len
+                if (ii+jj+kk)%2 ==0
+                    vel_p[i+1,j+1,k+1] = 6.5
+                    vel_s[i+1,j+1,k+1] = 3.8
+                else
+                    vel_p[i+1,j+1,k+1] = 5.5
+                    vel_s[i+1,j+1,k+1] = 3.2
+                end
+            end
+        end
+    end
+end
+
 ## build stations
 
 num_station = 50
@@ -44,55 +86,42 @@ stations = DataFrame(station_id=[], longitude=[], latitude=[], elevation_m=[], x
 
 for i in 1:num_station
     station_id = "NC.$(lpad(i, 2, '0'))"
-    latitude = config["latitude0"] + (rand() - 0.5) * 1
-    longitude = config["longitude0"] + (rand() - 0.5) * 1
+    latitude = config["latitude0"] + (rand() - 0.5) * config["latituderange"]
+    longitude = config["longitude0"] + (rand() - 0.5) * config["longituderange"]
     elevation_m = rand() * 1000
     depth_km = -elevation_m / 1000
     push!(stations, (station_id=station_id, longitude=longitude, latitude=latitude, elevation_m=elevation_m, x_km=missing, y_km=missing, z_km=depth_km))
 end
 
-pyproj = pyimport("pyproj") # use it in an environment with pyproj
-proj = pyproj.Proj(
-    "+proj=sterea +lon_0=$( (config["minlongitude"] + config["maxlongitude"]) / 2 ) +lat_0=$( (config["minlatitude"] + config["maxlatitude"]) / 2 ) +units=km"
-)
 stations.x_km, stations.y_km = proj(stations.longitude, stations.latitude)
 
-CSV.write("stations.csv", stations)
+CSV.write(folder * "/stations.csv", stations)
 
 ## build events
 
-num_event = 200
-events = DataFrame(event_index=[], longitude=[], latitude=[], magnitude=[], x_km = [], y_km = [], z_km = [])
+num_event = 100
+events = DataFrame(event_index=[], longitude=[], latitude=[], magnitude=[], time=[], x_km = [], y_km = [], z_km = [])
 index = 0
 for i in 1:num_event
     global index += 1 
-    latitude = config["latitude0"] + (rand() - 0.5) * 1
-    longitude = config["longitude0"] + (rand() - 0.5) * 1
+    latitude = config["latitude0"] + (rand() - 0.5) * config["latituderange"]
+    longitude = config["longitude0"] + (rand() - 0.5) * config["longituderange"]
     magnitude = rand()*2
     depth_km = config["depth0"] + (rand()-0.5) * 20
-    push!(events, (event_index=index, longitude=longitude, latitude=latitude, magnitude=magnitude, x_km=missing, y_km=missing, z_km=depth_km))
+    time = DateTime("2019-01-01T00:00:00", "yyyy-mm-ddTHH:MM:SS")
+    push!(events, (event_index=index, longitude=longitude, latitude=latitude, magnitude=magnitude, time=time, x_km=missing, y_km=missing, z_km=depth_km))
 end
 events.x_km, events.y_km = proj(events.longitude, events.latitude)
 
-CSV.write("events.csv", events)
+CSV.write(folder * "/events.csv", events)
 
 ## adjust locations for calculation
 
-h = 1.0; dx, dy = proj(config["minlongitude"],config["minlatitude"])
-dx = ceil(abs(dx)); stations.x = (stations.x_km .+ dx) ./ h .+ 1; events.x = (events.x_km .+ dx) ./ h .+ 1
-dy = ceil(abs(dy)); stations.y = (stations.y_km .+ dy) ./ h .+ 1; events.y = (events.y_km .+ dy) ./ h .+ 1
-stations.z = (stations.z_km .+ 1) ./ h .+ 1; events.z = events.z_km ./ h .+ 1
+stations.x = (stations.x_km .+ dx) ./ h .+ 1; events.x = (events.x_km .+ dx) ./ h .+ 1
+stations.y = (stations.y_km .+ dy) ./ h .+ 1; events.y = (events.y_km .+ dy) ./ h .+ 1
+stations.z = (stations.z_km .+ dz) ./ h .+ 1; events.z = (events.z_km .+ dz) ./ h .+ 1
 
-m, n = proj(config["maxlongitude"], config["maxlatitude"])
-m = convert(Int64,ceil((m + dx)/h)) + 1; n = convert(Int64, ceil((n + dy)/h)) + 1
-l = convert(Int64, ceil(config["maxdepth"]/h)) + 1
-
-## develop data of picks
-
-vel_p = ones(m,n,l) * 6.0; vel_s = ones(m,n,l) * 3.5 # (98, 113, 21)
-vel_p[40:60, 50:70, 8:14] .= 6.5; vel_s[40:60, 50:70, 8:14] .= 3.8
-
-up = PyObject[]; fvel_p = 1 ./ vel_p; fvel_s = 1 ./ vel_s
+up = PyObject[]; us = PyObject[]; fvel_p = 1 ./ vel_p; fvel_s = 1 ./ vel_s
 for i = 1:num_station
     ix = stations.x[i]; ixu = convert(Int64, ceil(ix)); ixd = convert(Int64, floor(ix))
     iy = stations.y[i]; iyu = convert(Int64, ceil(iy)); iyd = convert(Int64, floor(iy))
@@ -120,51 +149,74 @@ for i = 1:num_station
     u0[ixd,iyd,izd] = sqrt((ix-ixd)^2+(iy-iyd)^2+(iz-izd)^2)*fvel_s[ixd,iyd,izd]*h
     push!(us, eikonal3d(u0, fvel_s, h, m, n, l, 1e-3, false))
 end
+
 sess = Session(); init(sess)
 utravel_p = run(sess,up); utravel_s = run(sess,us)
-scaltime_p = ones(numsta,numeve); qua_p = ones(numsta,numeve)
-scaltime_s = ones(numsta,numeve); qua_s = ones(numsta,numeve)
-for i = 1:numsta
-    for j = 1:numeve
-        jx = alleve.x[j]; x1 = convert(Int64,floor(jx)); x2 = x1 + 1
-        jy = alleve.y[j]; y1 = convert(Int64,floor(jy)); y2 = y1 + 1
-        jz = alleve.z[j]; z1 = convert(Int64,floor(jz)); z2 = z1 + 1
+caltime_p = ones(num_station,num_event); qua_p = ones(num_station,num_event)
+caltime_s = ones(num_station,num_event); qua_s = ones(num_station,num_event)
+for i = 1:num_station
+    for j = 1:num_event
+        jx = events.x[j]; x1 = convert(Int64,floor(jx)); x2 = x1 + 1
+        jy = events.y[j]; y1 = convert(Int64,floor(jy)); y2 = y1 + 1
+        jz = events.z[j]; z1 = convert(Int64,floor(jz)); z2 = z1 + 1
         # P wave
-        txyz = (z2-jz)*(y2-jy)*((x2-jx)*ubeg_p[i][x1,y1,z1] + (jx-x1)*ubeg_p[i][x2,y1,z1]) +
-               (z2-jz)*(jy-y1)*((x2-jx)*ubeg_p[i][x1,y2,z1] + (jx-x1)*ubeg_p[i][x2,y2,z1]) + 
-               (jz-z1)*(y2-jy)*((x2-jx)*ubeg_p[i][x1,y1,z2] + (jx-x1)*ubeg_p[i][x2,y1,z2]) + 
-               (jz-z1)*(jy-y1)*((x2-jx)*ubeg_p[i][x1,y2,z2] + (jx-x1)*ubeg_p[i][x2,y2,z2])
-        scaltime_p[i,j] = txyz
+        txyz = (z2-jz)*(y2-jy)*((x2-jx)*utravel_p[i][x1,y1,z1] + (jx-x1)*utravel_p[i][x2,y1,z1]) +
+               (z2-jz)*(jy-y1)*((x2-jx)*utravel_p[i][x1,y2,z1] + (jx-x1)*utravel_p[i][x2,y2,z1]) + 
+               (jz-z1)*(y2-jy)*((x2-jx)*utravel_p[i][x1,y1,z2] + (jx-x1)*utravel_p[i][x2,y1,z2]) + 
+               (jz-z1)*(jy-y1)*((x2-jx)*utravel_p[i][x1,y2,z2] + (jx-x1)*utravel_p[i][x2,y2,z2])
+        caltime_p[i,j] = txyz
         qua_p[i,j] = rand()/2 + 0.5
         # S wave
-        txyz = (z2-jz)*(y2-jy)*((x2-jx)*ubeg_s[i][x1,y1,z1] + (jx-x1)*ubeg_s[i][x2,y1,z1]) +
-               (z2-jz)*(jy-y1)*((x2-jx)*ubeg_s[i][x1,y2,z1] + (jx-x1)*ubeg_s[i][x2,y2,z1]) + 
-               (jz-z1)*(y2-jy)*((x2-jx)*ubeg_s[i][x1,y1,z2] + (jx-x1)*ubeg_s[i][x2,y1,z2]) + 
-               (jz-z1)*(jy-y1)*((x2-jx)*ubeg_s[i][x1,y2,z2] + (jx-x1)*ubeg_s[i][x2,y2,z2])
-        scaltime_s[i,j] = txyz
+        txyz = (z2-jz)*(y2-jy)*((x2-jx)*utravel_s[i][x1,y1,z1] + (jx-x1)*utravel_s[i][x2,y1,z1]) +
+               (z2-jz)*(jy-y1)*((x2-jx)*utravel_s[i][x1,y2,z1] + (jx-x1)*utravel_s[i][x2,y2,z1]) + 
+               (jz-z1)*(y2-jy)*((x2-jx)*utravel_s[i][x1,y1,z2] + (jx-x1)*utravel_s[i][x2,y1,z2]) + 
+               (jz-z1)*(jy-y1)*((x2-jx)*utravel_s[i][x1,y2,z2] + (jx-x1)*utravel_s[i][x2,y2,z2])
+        caltime_s[i,j] = txyz
         qua_s[i,j]  = rand()/2 + 0.5
     end
 end
 
+## decide data of picks
+
+picks = DataFrame(station_id=[], phase_time=[], phase_score=[], phase_type=[], event_index=[])
+for i = 1:num_event
+    for j = 1:num_station
+        station_id = stations.station_id[j]
+        event_index = events.event_index[i]
+
+        type = "P"
+        time = events.time[i] + Millisecond(round(1000 * caltime_p[j,i]))
+        score = qua_p[j,i]
+        push!(picks,(station_id=station_id, phase_time=time, phase_score=score, phase_type=type, event_index=event_index))
+        type = "S"
+        time = events.time[i] + Millisecond(round(1000 * caltime_s[j,i]))
+        score = qua_s[j,i]
+        push!(picks,(station_id=station_id, phase_time=time, phase_score=score, phase_type=type, event_index=event_index))
+    end
+end
+
+CSV.write(folder * "/picks.csv", picks)
 
 
-# scaltime_d = scaltime_s - scaltime_p
+## matrix and files for inversion
 
-# if isfile(folder * "for_P/uobs_p.h5")
-#     rm(folder * "for_P/uobs_p.h5")
-#     rm(folder * "for_P/qua_p.h5")
-#     rm(folder * "for_S/uobs_s.h5")
-#     rm(folder * "for_S/qua_s.h5")
-#     rm(folder * "uobs_d.h5")
-# end
-# h5write(folder * "for_P/uobs_p.h5","matrix",scaltime_p)
-# h5write(folder * "for_S/uobs_s.h5","matrix",scaltime_s)
-# h5write(folder * "for_P/qua_p.h5","matrix",qua_p)
-# h5write(folder * "for_S/qua_s.h5","matrix",qua_s)
-# h5write(folder * "uobs_d.h5","matrix",scaltime_d)
+folder = folder * "/inv/"; 
+if !isfile(folder * "for_P/uobs_p.h5")
+    mkdir(folder * "for_P"); mkdir(folder * "for_S")
+end
 
-# # during inversion, vp can change in [-1,1], pvs can change in [-0.3,0.3]
-# # I suppose that Vp/Vs ratio almost equals an constant, and varies in some regions
-# # so Vp/Vs starts with this constant value, and I add a regression in the loss function
-# # just like L1 norm
-# # 
+rfile = open(folder * "range.txt","w")
+println(rfile,m);println(rfile,n);println(rfile,l);println(rfile,h)
+close(rfile)
+
+CSV.write(folder * "allsta.csv", stations)
+CSV.write(folder * "alleve.csv", events)
+
+if isfile(folder * "for_P/uobs_p.h5")
+    rm(folder * "for_P/uobs_p.h5"); rm(folder * "for_P/qua_p.h5")
+    rm(folder * "for_S/uobs_s.h5"); rm(folder * "for_S/qua_s.h5")
+    rm(folder * "uobs_d.h5")
+end
+h5write(folder * "for_P/uobs_p.h5","matrix",caltime_p); h5write(folder * "for_P/qua_p.h5","matrix",qua_p)
+h5write(folder * "for_S/uobs_s.h5","matrix",caltime_s); h5write(folder * "for_S/qua_s.h5","matrix",qua_s)
+caltime_d = caltime_s - caltime_p; h5write(folder * "uobs_d.h5","matrix",caltime_d)
